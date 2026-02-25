b76ac1eb75ba3e7a65defce773a0b2e4086acd42
Adekunle Muritala <adekunlemuritala@Mac.lan1>
2026-02-24 19:15:59 -0600

    cors

--- src/main/java/com/cuttypaws/security/CorsConfig.java
+++ src/main/java/com/cuttypaws/security/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://www.kinjomarket.com", "http://localhost:9494","http://localhost:5173")
+                        .allowedOrigins("https://www.cuttypaws.com", "http://localhost:9494","http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .exposedHeaders("Authorization")
